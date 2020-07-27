996b9ac8abdc74d06da6199cfa7ade322e926a82
joyce-shi <j224shi@uwaterloo.ca>
2020-07-27 02:03:28 -0400

    setup some boilerplate & removed readme

---
+++ frontend/components/Meta.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Meta = () => (
+    <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta charSet="utf-8" />
+        <link href="/static/favicon.png" rel="shortcut icon" />
+        <link href="/static/nprogress.css" rel="stylesheet" type="text/css" />
+        <title>Sick Fits!</title>
+    </Head>
+)
+
+export default Meta;
