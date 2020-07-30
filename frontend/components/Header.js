3a7bc9a896f82a47e509b2fc1277e769ea862d8e
joyce-shi <j224shi@uwaterloo.ca>
2020-07-30 15:32:24 -0400

    creating global styles & styling navbar

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
 import Nav from './Nav';
 import Link from 'next/link';
-import styled from 'styled-components';
 
 const Logo = styled.h1`
   font-size: 4rem;
