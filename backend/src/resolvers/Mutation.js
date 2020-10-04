519ba97bc68de3cf5ff484d8c38749c9fb3ae53b
joyce-shi <j224shi@uwaterloo.ca>
2020-10-03 21:26:49 -0400

    adding functionality to update and delete items in store

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -8,7 +8,27 @@ const Mutations = {
     }, info);
 
     return item;
+  },
+
+  updateItem(parent, args, context, info) {
+    const updates = { ...args };
+    delete updates.id;
+
+    return context.db.mutation.updateItem({
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      }, info
+    );
+  },
+
+  async deleteItem(parent, args, context, info) {
+    const where = { id: args.id };
+    const item = await context.db.query.item({ where }, `{ id title }`);
+    return context.db.mutation.deleteItem({ where }, info);
   }
+
 };
 
 module.exports = Mutations;
---
+++ frontend/pages/update.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import UpdateItem from '../components/CreateItem';
+
+const Sell = props => (
+    <div>
+        <UpdateItem />
+    </div>
+)
+
+export default Sell;
